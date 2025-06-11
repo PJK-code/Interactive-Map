104cea61f28f8c1826ca173cdcabf58bbbb818b9
Preston Kent <pkent1420@gmail.com>
2025-06-11 15:30:14 -0600

    Completed B

---
+++ D280-Task1/src/app/map-comp/map-comp.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map-comp',
+  standalone: true,
+  imports: [],
+  templateUrl: './map-comp.component.html',
+  styleUrl: './map-comp.component.css'
+})
+export class MapCompComponent {
+
+}
